a446c91f892b21074da2c4eaa8a0e32f97bbb589
ZaxarShashkov <zekharjah@gmail.com>
2023-06-28 11:54:05 +0300

    up

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -14,6 +14,8 @@ const PostsPage = () => {
 	const [limit, setLimit] = useState(100);
 	const [comment, setComment] = useState(0)
 	const [postsLocal, setPostsLocal] = useState([])
+	const [contentEditable, setContentEditable] = useState(false)
+	const [elected, setElected] = useState(false)
 
 	const { posts, isLoading, error } = useAppSelector((state) => state.postReducer);
 	const { users } = useAppSelector((state) => state.userReducer);
@@ -46,35 +48,37 @@ const PostsPage = () => {
 		setPostsLocal(postsLocal.filter((post) => post.id !== +e.currentTarget.dataset.id))
 	}
 
+	const editable = () => {
+		setContentEditable(contentEditable => !contentEditable)
+	}
+
 	return (
 		<Container>
 			<ButtonGroup handleClick={handleClick} />
 			<CardGroup >
-
 				<>
 					{users.slice(0, limit / 10).map((user) => {
 						const userPosts = postsLocal.slice(0, limit).filter((post) => post.userId === user.id);
 						return (
 							<div key={user.id}>
-								<Card.Title className='mb-3 mt-3'>Username: {user.name}</Card.Title>
+								<Card.Title className='mb-3 mt-3'><span contentEditable={contentEditable}>Username: {user.name}</span></Card.Title>
 								{userPosts.map((post) => {
 									const postComments = comments.filter((comment) => comment.postId === post.id)
 									return (
 										<Accordion defaultActiveKey="0" key={post.id}>
 											<Card key={post.id} className='mt-3'>
 												<Card.Body>
-													<Card.Title>{post.id}. {post.title}</Card.Title>
-													<Card.Text>Text: {post.body}</Card.Text>
+													<Card.Title><span contentEditable={contentEditable}>{post.id}. {post.title}</span></Card.Title>
+													<Card.Text><span contentEditable={contentEditable}>Text: {post.body}</span></Card.Text>
 													<div style={{ display: 'flex', gap: '1rem' }}>
 														<CustomToggle eventKey={post.id} id={post.id} setComment={setComment}>Comments</CustomToggle>
-														<Button variant="secondary">Edit</Button>
+														<Button variant="secondary" onClick={editable}>Edit</Button>
 														<Modal id={post.id} onRemove={onRemove}></Modal>
-														{/* <Button variant="secondary" data-id={post.id} onClick={(e) => onRemove(e)}>Remove</Button> */}
 													</div>
 												</Card.Body>
 												{postComments.map((comment) => (
 													<Accordion.Collapse eventKey={post.id} key={comment.id}>
-														<Card.Body>{comment.name}</Card.Body>
+														<Card.Body>Name: {comment.name}<br />Email: {comment.email}<br />Text: {comment.body}</Card.Body>
 													</Accordion.Collapse>
 												))}
 											</Card>
